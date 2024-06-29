b5041df4a8ba259287c0ded8f823690831528a3e
agarraga <83713047+agarraga@users.noreply.github.com>
2024-06-29 18:20:12 +0100

    better builtints

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,30 +6,30 @@
 /*   By: bob <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 14:39:28 by bob               #+#    #+#             */
-/*   Updated: 2024/05/28 15:13:24 by bob              ###   ########.fr       */
+/*   Updated: 2024/06/29 18:07:17 by algarrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <errno.h>
-#include "../mstypes.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "../../libft/ft.h"
 
-int	ft_builtin_exit(int argc, char **argv, int *lrs)
+/*
+ * The return of the value must be done outside exit if we want uniform
+ * behaviour in the builtins. This function does check that the arguments are
+ * valid, but the calling function should exit with the given argument as the
+ * status code.
+ */
+int	ft_exit(char *argv[], t_dlist **environ)
 {
-	if (argc > 2)
-	{
-		ft_putstr_fd("exit(builtin): ", 2);
-		errno = E2BIG;
-		return (E2BIG);
-	}
-	if (argc == 1)
-		return (MS_ERR_OK);
+	(void) environ;
+	if (argv[1] && argv[2])
+		exit(E2BIG);
+	if (!argv[1])
+		exit(0);
 	if (!ft_stris(argv[1], &ft_isdigit))
-	{
-		ft_putstr_fd("exit(builtin): ", 2);
-		errno = EINVAL;
-		return (EINVAL);
-	}
-	*lrs = ft_atoi(argv[1]);
-	return (MS_ERR_OK);
+		exit(EINVAL);
+	printf("exit\n");
+	exit(0);
 }
