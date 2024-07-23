2b30355968d33fd762ad1f4c34938624358184e1
Sergio Rodrigo De Los Rios <srodrigo@c2r3s3.42london.com>
2024-07-23 18:09:53 +0100

    norm

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: srodrigo <srodrigo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 17:47:21 by algarrig          #+#    #+#             */
-/*   Updated: 2024/07/23 18:02:50 by srodrigo         ###   ########.fr       */
+/*   Updated: 2024/07/23 18:09:13 by srodrigo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@
 
 bool	ft_parse(t_dlist *tokens)
 {
-
 	if (tokens == NULL)
 		return (false);
 	if (is_command_line(&tokens))
